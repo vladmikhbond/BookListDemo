26895ffa3bc89f4daa0bce03e2439afaa11ce126
volodymyr bondariev <vladmikhbond@hotmail.com>
2019-03-07 18:22:21 +0200

    Add project files.

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        public Book(int id, string authors, string title)
+        {
+            Id = id;
+            Authors = authors;
+            Title = title;
+        }
+
+        public int Id { set; get; }
+        public string Title { set; get; }
+        public string Authors { set; get; }
+    }
+}
---
+++ BookLibrary/Library.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BookLibrary
+{
+    public class Library
+    {
+        public List<Book> Books { set; get; }
+        public string PathToFile { set; get; }
+
+        public void LoadFromFile()
+        {
+            Books = new List<Book>();
+            using (TextReader reader = new StreamReader(File.OpenRead(PathToFile)))
+            {
+                string s = null;
+                while ((s = reader.ReadLine()) != null)
+                {
+                    int id = Convert.ToInt32(s);
+                    var title = reader.ReadLine();
+                    var authors = reader.ReadLine();
+                    Books.Add(new Book(id, authors, title));
+                }
+            }
+        }
+    }
+}
