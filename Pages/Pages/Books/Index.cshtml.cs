d31aa1cf4d3518225d38e1ac97b3a963f9fca78e
volodymyr bondariev <vladmikhbond@hotmail.com>
2019-03-12 20:30:04 +0200

    pages in process

--- Pages/Pages/Books/Index.cshtml.cs
+++ Pages/Pages/Books/Index.cshtml.cs
@@ -6,7 +6,7 @@ using BookLibrary;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Pages.Pages
+namespace Pages.Pages.Books
 {
     public class IndexModel : PageModel
     {
