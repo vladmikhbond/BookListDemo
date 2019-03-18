b898e68621db77da2f702fd7b4feaf64e90c8f53
volodymyr bondariev <vladmikhbond@hotmail.com>
2019-03-18 16:30:50 +0200

    Pages & ajax

---
+++ Pages/Pages/Books/Index2.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pages.Pages.Books
+{
+    public class Index2Model : PageModel
+    {
+        readonly Library _lib;
+
+        public List<Book> Books { set; get; }
+
+        public Index2Model(Library lib)
+        {
+            _lib = lib;
+            Books = new List<Book>(lib.Books);
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
