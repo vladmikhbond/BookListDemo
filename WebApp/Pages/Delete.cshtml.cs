bd36b224b1c556a11482d829203672cd5007d0e6
volodymyr bondariev <vladmikhbond@hotmail.com>
2019-03-21 21:56:30 +0200

    WebApp draft

---
+++ WebApp/Pages/Delete.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+using WebApp.Services;
+
+namespace WebApp.Pages
+{
+
+    public class DeleteModel : PageModel
+    {
+        readonly Library _lib;
+
+        public DeleteModel(Library lib)
+        {
+            _lib = lib;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string Id { set; get; }
+
+        [BindProperty]
+        [Required]
+        public string Title { set; get; }
+        [BindProperty]
+        [Required]
+        public string Authors { set; get; }
+
+        public void OnGet()
+        {
+            Book book = _lib.Get().SingleOrDefault(b => b.Id == Id);
+            if (book != null)
+            {
+                Title = book.Title;
+                Authors = book.Authors;
+            }
+        }
+
+        public IActionResult OnPostAsync()
+        {
+            Book book = _lib.Get().SingleOrDefault(b => b.Id == Id);
+            if (book != null)
+            {
+                _lib.Remove(book.Id);
+            }
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ WebApp/Pages/Edit.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+using WebApp.Services;
+
+namespace WebApp.Pages
+{
+
+    public class EditModel : PageModel
+    {
+        readonly Library _lib;
+
+        public EditModel(Library lib)
+        {
+            _lib = lib;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string Id { set; get; }
+
+        [BindProperty]
+        [Required]
+        public string Title { set; get; }
+        [BindProperty]
+        [Required]
+        public string Authors { set; get; }
+
+        public void OnGet()
+        {
+            Book book = _lib.Get(Id);
+            if (book != null)
+            {               
+                Title = book.Title;
+                Authors = book.Authors;
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid) {
+                var newBook = new Book { Authors = Authors, Id = Id, Title = Title };
+                _lib.Update(Id, newBook);
+                return RedirectToPage("/Index");
+            }
+            return Page();
+        }
+    }
+}
